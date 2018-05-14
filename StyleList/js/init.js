2a70b42951b7ad37e18aeb3b320ac98806e639ae
captain <30402258+Deepthouguts@users.noreply.github.com>
2018-05-14 19:08:56 +0800

    key 测试

--- StyleList/js/init.js
+++ StyleList/js/init.js
@@ -4,6 +4,26 @@
     Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
+
+<span style="white-space:pre;"> 
+</span><span style="font-size:14px;">var width=document.documentElement.clientWidth;//获取屏幕的宽度  
+    var height=document.documentElement.clientHeight;//获取屏幕的高度  
+    var bili1=width/750;//屏幕宽度与设计稿宽度的比例（750是设计稿的宽度）  
+    var bili2=height/(1336-48);//屏幕高度与设计稿高度的比例(1336是设计稿的高度，其中48是设计稿中含有手机顶部的状态栏，需要去掉，1366-48才是真正需要显示的东西)  
+    var bili=bili1<bili2?bili1:bili2;//宽度的比例和高度的比例进行比较，取值最小的  
+    var html = document.querySelector('html');//选择html节点  
+    var rem = 16;//手动设置rem与px的比例；  
+    html.style.fontSize = rem + "px";//设置html的默认fontsize为16px。(注意，浏览器中最小值为12px，)  
+    var __bili=bili/rem;//将比例和rem进行联系。  
+    document.documentElement.style.setProperty('--bili', __bili+"rem");//设置css中的变量为--bili，值为__bili</span>  
+	
+	
+	
+	
+	
+	
+	
+	
 skel.init({
 	prefix: 'css/style',
 	resetCSS: true,
